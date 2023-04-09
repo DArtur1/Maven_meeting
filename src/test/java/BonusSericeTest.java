9587a5372e29ed2bc41f422e116a557d435dde91
DudkaAV_QA <arturdudka@yandex.ru>
2023-04-09 16:45:27 +0700

    add autotest

--- src/test/java/BonusSericeTest.java
+++ src/test/java/BonusSericeTest.java
@@ -4,12 +4,23 @@ import ru.netology.javaqa.BonusService;
 
 public class BonusSericeTest {
     @Test
-    public void test() {
+    public void testRegisteredUnderLimit() {
         BonusService service = new BonusService();
 
         int expected = 150;
         int actual = service.calxBonus(5000, true);
         //System.out.println("1. " + expected + " == ? == " + actual);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testUnregisteredUnderLimit(){
+        BonusService service = new BonusService();
+
+        int expected = 50;
+        int actual = service.calxBonus(5000, false);
+        //System.out.println("2. " + expected + " == ? == " + actual);
 
         Assertions.assertEquals(expected, actual);
     }
